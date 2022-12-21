6542013b4b2dd5d8e247c0b35eeebcefe21efcf8
SON.NGUYEN8 <son.nguyen8@mservice.com.vn>
2022-12-21 17:51:19 +0700

    [EventBus] Create HeatSensor

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,22 @@ dependencies {
 
     implementation("io.vertx:vertx-core:$vertxVersion")
     implementation("io.vertx:vertx-web:$vertxVersion")
+    implementation("io.vertx:vertx-codegen:$vertxVersion")
+    implementation("io.vertx:vertx-service-proxy:$vertxVersion")
+
     implementation("ch.qos.logback:logback-classic:1.4.4")
     implementation("io.vertx:vertx-rx-java3:$vertxVersion")
+
+    annotationProcessor("io.vertx:vertx-service-proxy:$vertxVersion")
+    annotationProcessor("io.vertx:vertx-codegen:$vertxVersion:processor")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_13
+}
+
+tasks.getByName<JavaCompile>("compileJava") {
+    // This allows you to customize where the files are being generated.
+    options.annotationProcessorGeneratedSourcesDirectory =
+        File("$projectDir/src/main/generated")
 }
---
+++ src/main/java/org/example/eventbus/sensor/HeatSensor.java
@@ -0,0 +1,50 @@
+package org.example.eventbus.sensor;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+import java.util.Random;
+import java.util.UUID;
+
+public class HeatSensor extends AbstractVerticle {
+
+    private final Random random = new Random();
+    private final String sensorId = UUID.randomUUID().toString();
+    private double temperature = 28.0;
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(HeatSensor.class.getName(), new DeploymentOptions().setInstances(4));
+        vertx.deployVerticle(Listener.class.getName());
+        vertx.deployVerticle(SensorData.class.getName());
+        vertx.deployVerticle(HttpServer.class.getName());
+    }
+
+    @Override
+    public void start() {
+        scheduleNextUpdate();
+    }
+
+    private void scheduleNextUpdate() {
+        vertx.setTimer(random.nextInt(5000) + 1000, this::update);
+    }
+
+    private void update(long timerId) {
+        temperature = temperature + (delta() / 10);
+        JsonObject payload = new JsonObject()
+                .put("id", sensorId)
+                .put("temp", temperature);
+        vertx.eventBus().publish("sensor.updates", payload);
+        scheduleNextUpdate();
+    }
+
+    private double delta() {
+        if (random.nextInt() > 0) {
+            return random.nextGaussian();
+        } else {
+            return -random.nextGaussian();
+        }
+    }
+}
